f102bf9711a6d89668e9cf6a95770ab1dc4eb362
Eduardo Silva <135537993+EduardoSilva1608@users.noreply.github.com>
2024-05-15 11:13:23 -0300

    fim

--- _js/addProduto.js
+++ _js/addProduto.js
@@ -1,9 +1,10 @@
-var idCount = 1; // Variável para contar o ID incremental
+// Variável para contar o ID incremental
+var idCount = 1;
 
 // Função para validar o produto (converte para maiúsculas)
 function validarProduto() {
     var produtoInput = document.getElementById('produto');
-    produtoInput.value = produtoInput.value.toUpperCase();
+    produtoInput.value = produtoInput.value.toUpperCase().trim();
 }
 
 // Função para validar a quantidade (ajusta para 1 se for <= 0)
@@ -16,64 +17,50 @@ function validarQuantidade() {
     }
 }
 
+function permitirSomenteNumeros(input) {
+    input.value = input.value.replace(/[^0-9]/g, '');
+}
+
 // Função para validar o valor (ajusta para 1 se for <= 0)
 function validarValor() {
     var valorInput = document.getElementById('valor');
     var valor = parseFloat(valorInput.value.replace(',', '.'));
 
     if (isNaN(valor) || valor <= 0) {
-        valorInput.value = '1';
+        valorInput.value = '1,00';
     }
 }
 
 // Função para validar e adicionar o item
 function validarEAdicionarItem() {
-    // Executa as validações
     validarProduto();
     validarQuantidade();
     validarValor();
 
-    // Obtém os valores dos campos
     var produto = document.getElementById('produto').value;
     var qtd = document.getElementById('qtd').value;
     var valor = document.getElementById('valor').value;
 
-    // Se todos os campos estiverem preenchidos, chama a função adicionarItem()
-    if (produto.trim() === '' || qtd.trim() === '' || valor.trim() === '') {
+    if (!produto || !qtd || !valor) {
         alert('Por favor, preencha todos os campos antes de adicionar o produto.');
         return;
     }
 
-    // Se todos os campos estiverem preenchidos e validados, chama a função adicionarItem()
     adicionarItem();
 }
 
 // Função para excluir um item
 function excluirItem(id) {
-    // Obtém o elemento do item pelo ID
     var item = document.getElementById('item-' + id);
-
-    // Exibe o modal de confirmação
     var modal = document.getElementById('modalExclusao');
     modal.style.display = 'block';
 
-    // Define ação para o botão "Confirmar"
     document.getElementById('confirmarExclusao').onclick = function () {
-        // Remove o item da lista
         item.parentNode.removeChild(item);
-
-        // Atualiza o valor total
-        var valorItem = parseFloat(item.querySelector('p:nth-child(4)').textContent.replace(/[^\d,]/g, '').replace(',', '.'));
-        var qtdItem = parseInt(item.querySelector('p:nth-child(3)').textContent);
-        var totalValor = parseFloat(document.getElementById('totalValor').textContent.replace(/[^\d,]/g, '').replace(',', '.'));
-        totalValor -= valorItem * qtdItem;
-        document.getElementById('totalValor').textContent = totalValor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
-        // Fecha o modal
+        atualizarValorTotal();
         modal.style.display = 'none';
     };
 
-    // Define ação para o botão "Fechar" do modal
     var closeBtn = document.getElementsByClassName('close')[0];
     closeBtn.onclick = function () {
         modal.style.display = 'none';
@@ -82,54 +69,107 @@ function excluirItem(id) {
 
 // Função para adicionar um novo item
 function adicionarItem() {
-    // Obtém os valores dos campos
     var produto = document.getElementById('produto').value.trim();
     var qtd = parseInt(document.getElementById('qtd').value);
-    var valorInput = document.getElementById('valor');
-    var valor = parseFloat(valorInput.value.replace(/[^\d,]/g, '').replace(',', '.'));
+    var valor = parseFloat(document.getElementById('valor').value.replace(',', '.'));
+    var valorTotalItem = valor * qtd; // Multiplica o valor pela quantidade
 
-    // Cria um novo elemento div para o item
     var newItem = document.createElement('div');
     newItem.classList.add('item');
-    newItem.id = 'item-' + idCount; // Define um ID para o item
+    newItem.id = 'item-' + idCount;
 
-    // Adiciona o ID incremental ao novo item
-    var id = idCount;
-    idCount++; // Incrementa o contador de ID
+    var id = idCount++;
+    newItem.innerHTML = `
+        <p>${id}</p>
+        <p>${produto}</p>
+        <p>${qtd}x</p>
+        <p>${valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+        <p style='display:none'>${valorTotalItem.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+        <button onclick="excluirItem(${id})">Excluir</button>
+    `;
 
-    // Monta o HTML do novo item
-    newItem.innerHTML = '<p>' + id + '</p><p>' + produto + '</p><p>' + qtd + 'x</p><p>' + valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + '</p><button onclick="excluirItem(' + id + ')">Excluir</button>';
-
-    // Adiciona o novo item à lista de itens
     document.getElementById('listaItens').appendChild(newItem);
+    atualizarValorTotal();
 
-    // Atualiza o valor total
-    var totalValor = parseFloat(document.getElementById('totalValor').textContent.replace(/[^\d,]/g, '').replace(',', '.'));
-    totalValor += valor * qtd;
-    document.getElementById('totalValor').textContent = totalValor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
-    // Verifica se a lista de itens está vazia para exibir a seção
     if (document.querySelectorAll('.item').length === 1) {
         document.getElementById('listaItens').parentNode.style.display = 'block';
     }
 
-    // Limpa os campos de entrada
+    limparCampos();
+}
+
+// Função para atualizar o valor total
+function atualizarValorTotal() {
+    var totalValor = 0;
+    var items = document.querySelectorAll('.item');
+    items.forEach(function (item) {
+        var valorTotalItem = parseFloat(item.querySelector('p:nth-child(5)').textContent.replace(/[^\d,]/g, '').replace(',', '.'));
+        totalValor += valorTotalItem;
+    });
+    document.getElementById('totalValor').textContent = totalValor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+
+// Função para limpar os campos de entrada
+function limparCampos() {
     document.getElementById('produto').value = '';
     document.getElementById('qtd').value = '';
     document.getElementById('valor').value = '';
-
-    // Define o foco de volta no campo de entrada do produto
     document.getElementById('produto').focus();
 }
 
-// Adicionando evento de tecla ao campo de valor para permitir adicionar item pressionando "Enter"
+// Evento de tecla ao campo de valor para permitir adicionar item pressionando "Enter"
 document.getElementById('valor').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         validarEAdicionarItem();
     }
 });
 
-// Adiciona um evento de confirmação antes de recarregar a página
+// Evento de confirmação antes de recarregar a página
 window.onbeforeunload = function () {
     return "Tem certeza que deseja sair desta página?";
 };
+
+// Função para enviar a lista de compras via WhatsApp
+function enviarMensagemWhatsApp() {
+    // Obtém os itens da lista
+    var itens = document.querySelectorAll('.item');
+
+    // Verifica se há produtos na lista
+    if (itens.length === 0) {
+        alert("Não há produtos na lista para enviar via WhatsApp.");
+        return;
+    }
+
+    // Inicializa a mensagem com um texto introdutório
+    var mensagem = "Lista de Compras:\n";
+
+    // Inicializa o valor total
+    var total = 0;
+
+    // Percorre cada item e adiciona à mensagem
+    itens.forEach(function (item) {
+        var produto = item.querySelector('p:nth-child(2)').textContent;
+        var qtd = item.querySelector('p:nth-child(3)').textContent;
+        var valorUnitario = item.querySelector('p:nth-child(4)').textContent;
+        var valorTotalItem = item.querySelector('p:nth-child(5)').textContent;
+
+        // Adiciona o produto, quantidade e valor total à mensagem
+        mensagem += `${produto} (${qtd}), ${valorTotalItem}\n`;
+
+        // Atualiza o valor total
+        total += parseFloat(valorTotalItem.replace(/[^\d,]/g, '').replace(',', '.'));
+    });
+
+    // Formata o valor total no formato desejado
+    var valorTotalFormatado = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Formata a mensagem para incluir o valor total
+    mensagem += `\n*Total: ${valorTotalFormatado}*`;
+
+    // Formata a mensagem para que possa ser enviada via WhatsApp
+    mensagem = encodeURIComponent(mensagem);
+
+    // Abre o WhatsApp com a mensagem pré-preenchida
+    window.open(`https://api.whatsapp.com/send?text=${mensagem}`);
+}
